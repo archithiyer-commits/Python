19e665d2231bd68ae7308e41c9d50e06a1b00f8c
archithiyer-commits <archith.iyer@gmail.com>
2025-11-09 15:38:33 +0530

    Add files via upload

---
+++ import tkinter as tk.py
@@ -0,0 +1,9 @@
+import tkinter as tk
+from tkinter import messagebox
+
+# Setting up Main Window
+root = tk.Tk()
+root.title('Length converter app')
+root.configure(bg='light blue')
+root.geometry('400x400')
+root.mainloop()
